2771001d989d8ba76526ff76628c77331e34f4bd
PizzaCrust <pizzacrustcoder@gmail.com>
2017-03-18 12:41:37 +0700

    Import zipextractor

---
+++ src/main/java/iodine/ZipExtractor.java
@@ -0,0 +1,59 @@
+package iodine;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.function.Predicate;
+import java.util.zip.ZipFile;
+
+public class ZipExtractor {
+
+    private final Predicate<String> compatibleNamePredicate;
+    private final ZipFile zipFile;
+    private final File file;
+
+    public ZipExtractor(Predicate<String> compatibleNamePredicate, File file) throws IOException {
+        this.compatibleNamePredicate = compatibleNamePredicate;
+        zipFile = new ZipFile(file);
+        this.file = file;
+    }
+
+    public void extract(File outputDirectory, boolean deleteFile) {
+        zipFile.stream().forEach((zipEntry) -> {
+            if (compatibleNamePredicate.test(zipEntry.getName())) {
+                File outputFile = new File(outputDirectory, zipEntry.getName());
+                if (outputFile.getParentFile() != null) {
+                    outputFile.getParentFile().mkdirs();
+                } else {
+                    throw new RuntimeException("Parent file null.");
+                }
+                try {
+                    FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+                    ReadableByteChannel streamChannel = Channels.newChannel(zipFile
+                            .getInputStream(zipEntry));
+                    fileOutputStream.getChannel().transferFrom(streamChannel, 0, Long.MAX_VALUE);
+                    streamChannel.close();
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        try {
+            zipFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.gc();
+        if (deleteFile) {
+            if (!file.delete()) {
+                throw new RuntimeException("Failed to delete.");
+            }
+        }
+    }
+
+
+}
