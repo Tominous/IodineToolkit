40ff133656e8d6ea6bdf412b79b502f50da91793
PizzaCrust <pizzacrustcoder@gmail.com>
2017-03-18 20:42:18 +0700

    Gradle properly formatss

--- src/main/java/iodine/maven/GradleScriptBuilder.java
+++ src/main/java/iodine/maven/GradleScriptBuilder.java
@@ -3,6 +3,8 @@ package iodine.maven;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,13 +14,13 @@ public class GradleScriptBuilder {
     private String groupId = null;
     private String version = null;
 
-    private final Map<String, DependencyData> dependencies = new HashMap<>();
+    private final Map<DependencyData, String> dependencies = new HashMap<>();
     private final List<RepositoryData> repositories = new ArrayList<>();
 
     private final List<String> plugins = new ArrayList<>();
 
     public void addDependency(String scope, DependencyData dependencyData) {
-        dependencies.put(scope, dependencyData);
+        dependencies.put(dependencyData, scope);
     }
 
     public GradleScriptBuilder addPlugin(String plugin) {
@@ -66,7 +68,15 @@ public class GradleScriptBuilder {
         pomData.getDependencies().forEach(this::addCompileDependency);
         this.groupId = pomData.getGroupId();
         this.version = pomData.getVersion();
+        addPlugin("java");
         return this;
+    }
+
+    public static void main(String... args) throws Exception {
+        POMData pomData = new POMData("net.minecraft.server", "nms", "1.11", Arrays.asList
+                (DependencyData.DEFAULT_DEPENDENCIES), Collections.singletonList(RepositoryData
+                .MOJANG_REPOSITORY));
+        System.out.println(new GradleScriptBuilder().importPom(pomData).create());
     }
 
     public String create() {
@@ -74,20 +84,24 @@ public class GradleScriptBuilder {
         PrintWriter printWriter = new PrintWriter(stringWriter);
         printWriter.println("group = '" + this.groupId + "'");
         printWriter.println("version = '" + this.version + "'");
+        printWriter.println();
         plugins.forEach((pluginName) -> printWriter.println("apply plugin: '" + pluginName + "'"));
+        printWriter.println();
         printWriter.println("repositories {");
         repositories.forEach((repositoryData -> {
-            printWriter.println("maven {");
-            printWriter.println("   name = '" + repositoryData.id + "'");
-            printWriter.println("   url = '" + repositoryData.url + "'");
-            printWriter.println("}");
+            printWriter.println("    maven {");
+            printWriter.println("       name = '" + repositoryData.id + "'");
+            printWriter.println("       url = '" + repositoryData.url + "'");
+            printWriter.println("    }");
         }));
         printWriter.println("}");
+        printWriter.println();
         printWriter.println("dependencies {");
-        dependencies.forEach(((scope, dependencyData) -> {
-            printWriter.println(scope + " " + "'" + dependencyData.groupId + ":" + dependencyData
-                    .artifactId + ":" + dependencyData.version + "'");
-        }));
+        dependencies.forEach(((dependencyData, scope) -> printWriter.println("    " + scope + " " +
+                "'" +
+                dependencyData.groupId + ":" +
+        dependencyData
+                .artifactId + ":" + dependencyData.version + "'")));
         printWriter.println("}");
         return stringWriter.toString();
     }
